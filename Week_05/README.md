1386225abe1d1c4557047e916219c5d4d66c9551
zhangmx <icediv@163.com>
2020-11-17 21:43:48 +0800

    w5h1

--- Week_05/README.md
+++ Week_05/README.md
@@ -1,1 +1,17 @@
-学习笔记
+# 学习笔记
+
+## 相关资料：
+
+https://github.com/apache/shardingsphere
+https://gitee.com/nicefish/nicefish-backend
+
+## 其他
+git clone 报错：error: unable to create file Filename too long
+```
+git config --global core.longpaths true
+```
+或
+```
+[core]
+	longpaths = true
+```
---
+++ Week_05/mustDo/springBeans/spring-beans-init/src/main/java/com/zmx/springbeansinit/SpringBeansInitApplication.java
@@ -0,0 +1,13 @@
+package com.zmx.springbeansinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeansInitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeansInitApplication.class, args);
+	}
+
+}
